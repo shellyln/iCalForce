678a07703c8d00c032e433bae4214cb269d61baf
qnq777 <qnq777@users.noreply.github.com>
2013-10-21 20:58:44 +0900

    update setup script.

--- iCalForce/setup.sh
+++ iCalForce/setup.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-cd ./vendor
-git clone https://github.com/developerforce/Force.com-Toolkit-for-PHP.git soapclient.repo
-git clone https://github.com/adoy/PHP-OAuth2.git PHP-OAuth2
-cd ..
+echo '[IN] ./vendor'
+cd ./vendor || { echo error; exit 1; }
+
+echo 'Removing vendor libraries...'
+rm -rf soapclient.repo || { echo error; exit 1; }
+rm -rf PHP-OAuth2      || { echo error; exit 1; }
+echo 'finished!'
+
+echo 'Fetching vendor libraries...'
+git clone https://github.com/developerforce/Force.com-Toolkit-for-PHP.git soapclient.repo || { echo error; exit 1; }
+git clone https://github.com/adoy/PHP-OAuth2.git PHP-OAuth2                               || { echo error; exit 1; }
+echo 'finished!'
+
+echo 'Removing vendor .git directories...'
+rm -rf soapclient.repo/.git || { echo error; exit 1; }
+rm -rf PHP-OAuth2/.git      || { echo error; exit 1; }
+echo 'finished!'
+
+echo '[OUT] ./vendor'
+cd .. || { echo error; exit 1; }
+
+if [ -d ../.git ]; then
+  echo "it is Git managed."
+  git fetch --all || { echo error; exit 1; }
+  git status      || { echo error; exit 1; }
+fi
+
+echo 'finished!'
